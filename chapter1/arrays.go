e3692a061863748640e67533f84fc13f33c5ef5e
Mauricio Rojas <mrojasb2000@gmail.com>
2018-10-11 10:37:13 -0300

    Add declaration and initialization arrays structures.

---
+++ chapter1/arrays.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	var arr1 [100]int
+	arr2 := [3]string{"go", "is", "awesome"}
+	var arr3 [2]bool
+	arr3[0] = true
+	// zero-initialization by default is false
+	//arr3[1] = false
+}
