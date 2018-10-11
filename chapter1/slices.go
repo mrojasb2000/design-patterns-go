63710ab05ea96ca2398d7ce71d108cff90469f8c
Mauricio Rojas <mrojasb2000@gmail.com>
2018-10-11 10:49:56 -0300

    Add declaration and initialization slices structures.

---
+++ chapter1/slices.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mySlice := make([]int, 10) // Create an underlying array of the ten elements.
+	fmt.Println(len(mySlice), ":", cap(mySlice))
+	mySlice = append(mySlice, 5) // Change the size of the Slice, add a new number.
+	fmt.Println(len(mySlice), ":", cap(mySlice))
+
+	mySlice = mySlice[1:] // Delete first element from the Slice
+}
