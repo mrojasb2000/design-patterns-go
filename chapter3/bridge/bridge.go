ffdb2aeb08df2e8e47603fd11ad20cea10cbbccf
Mauricio Rojas <mrojasb2000@gmail.com>
2018-11-14 17:35:26 -0300

    Implement Bridge pattern and examples

--- chapter3/bridge/bridge.go
+++ chapter3/bridge/bridge.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 )
 
@@ -14,7 +15,8 @@ type PrinterAPI interface {
 type PrinterImpl1 struct{}
 
 func (p *PrinterImpl1) PrintMessage(msg string) error {
-	return errors.New("Not implemented yet")
+	fmt.Printf("%s\n", msg)
+	return nil
 }
 
 type PrinterImpl2 struct {
@@ -22,5 +24,35 @@ type PrinterImpl2 struct {
 }
 
 func (d *PrinterImpl2) PrintMessage(msg string) error {
+	if d.Writer == nil {
+		return errors.New("You need to pass an io.Writer to PrinterImpl2")
+	}
+	fmt.Fprintf(d.Writer, "%s", msg)
+	return nil
+}
+
+// acceptance criteria 4
+type PrinterAbstraction interface {
+	Print() error
+}
+
+// acceptance criteria 5
+type NormalPrinter struct {
+	Msg     string
+	Printer PrinterAPI
+}
+
+func (c *NormalPrinter) Print() error {
+	c.Printer.PrintMessage(c.Msg)
+	return nil
+}
+
+// acceptance criteria 6
+type PacktPrinter struct {
+	Msg     string
+	Printer PrinterAPI
+}
+
+func (c *PacktPrinter) Print() error {
 	return errors.New("Not implemented yet")
 }
