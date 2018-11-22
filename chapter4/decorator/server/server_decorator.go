49c40e93022d1ba9a9a0b0093bd77854bc304d84
Mauricio Rojas <mrojasb2000@gmail.com>
2018-11-22 09:50:00 -0300

    Implement LogMiddlerware and Basic Authentication Middlerware

--- chapter4/decorator/server/server_decorator.go
+++ chapter4/decorator/server/server_decorator.go
@@ -20,7 +20,7 @@ type LoggerServer struct {
 }
 
 func (s *LoggerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(s.LogWriter, "Request URI.  : %s\n", r.RequestURI)
+	fmt.Fprintf(s.LogWriter, "\nRequest URI.  : %s\n", r.RequestURI)
 	fmt.Fprintf(s.LogWriter, "Host          : %s\n", r.Host)
 	fmt.Fprintf(s.LogWriter, "Content Length: %d\n", r.ContentLength)
 	fmt.Fprintf(s.LogWriter, "Method        : %s\n", r.Method)
@@ -29,13 +29,45 @@ func (s *LoggerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.Handler.ServeHTTP(w, r)
 }
 
+type BasicAuthMiddleware struct {
+	Handler  http.Handler
+	User     string
+	Password string
+}
+
+func (s *BasicAuthMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user, pass, ok := r.BasicAuth()
+
+	if ok {
+		if user == s.User && pass == s.Password {
+			s.Handler.ServeHTTP(w, r)
+		} else {
+			fmt.Fprintf(w, "User or password incorrect\n")
+		}
+	} else {
+		fmt.Fprintf(w, "Error trying to retrive data from Basic auth\n")
+	}
+}
+
 func main() {
 	//http.Handle("/", &MyServer{})
 
-	http.Handle("/", &LoggerServer{
+	/*
+		http.Handle("/", &LoggerServer{
+			LogWriter: os.Stdout,
+			Handler:   &MyServer{},
+		})*/
+
+	mySuperServer := &LoggerServer{
+		Handler: &BasicAuthMiddleware{
+			Handler:  new(MyServer),
+			User:     "admin",
+			Password: "1234",
+		},
 		LogWriter: os.Stdout,
-		Handler:   &MyServer{},
-	})
+	}
+
+	http.Handle("/", mySuperServer)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
