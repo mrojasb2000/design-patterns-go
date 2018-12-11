764f831ba6810710b78bc009064d1167f9503147
Mauricio Rojas <mrojasb2000@gmail.com>
2018-12-10 21:50:00 -0300

    Flyweight patterns ans Unit Test.

---
+++ chapter4/flyweight/flyweight.go
@@ -0,0 +1,85 @@
+package flyweight
+
+import (
+	"time"
+)
+
+const (
+	TEAM_A = iota
+	TEAM_B
+)
+
+// Acceptance Criteria 1
+type Team struct {
+	ID             uint64
+	Name           string
+	Shiled         []byte
+	Players        []Player
+	HistoricalData []HistoricalData
+}
+
+type Player struct {
+	Name         string
+	Surname      string
+	PreviousTeam uint64
+	Photo        []byte
+}
+
+type HistoricalData struct {
+	Year          uint8
+	LeagueResults []Match
+}
+
+type Match struct {
+	Date          time.Time
+	VisitorID     uint64
+	LocalID       uint64
+	LocalScore    byte
+	VisitorScore  byte
+	LocalShoots   uint16
+	VisitorShoots uint16
+}
+
+// Acceptance Criteria 2
+type teamFlyweightFactory struct {
+	createdTeams map[int]*Team
+}
+
+// Simple Factory Builder
+func NewTeamFactory() teamFlyweightFactory {
+	return teamFlyweightFactory{
+		createdTeams: make(map[int]*Team),
+	}
+}
+
+func (t *teamFlyweightFactory) GetTeam(teamID int) *Team {
+	if t.createdTeams[teamID] != nil {
+		return t.createdTeams[teamID]
+	}
+
+	team := getTeamFactory(teamID)
+
+	t.createdTeams[teamID] = &team
+
+	return t.createdTeams[teamID]
+}
+
+func (t *teamFlyweightFactory) GetNumberOfObjects() int {
+	return len(t.createdTeams)
+}
+
+// Real Factory
+func getTeamFactory(team int) Team {
+	switch team {
+	case TEAM_B:
+		return Team{
+			ID:   2,
+			Name: "TEAM_B",
+		}
+	default:
+		return Team{
+			ID:   1,
+			Name: "TEAM_A",
+		}
+	}
+}
