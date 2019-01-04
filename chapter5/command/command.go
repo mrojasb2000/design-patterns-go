125c2da543033c2228f96bae89f83c1ab4fda65f
Mauricio Rojas <mrojasb2000@gmail.com>
2019-01-04 15:22:16 -0300

    Added command constructor signature

--- chapter5/command/command.go
+++ chapter5/command/command.go
@@ -12,6 +12,13 @@ func (c *ConsoleOutput) Execute() {
 	fmt.Println(c.message)
 }
 
+func CreateCommand(s string) Command {
+	fmt.Println("Creating command")
+	return &ConsoleOutput{
+		message: s,
+	}
+}
+
 func main() {
 	println("Hello World!")
 }
