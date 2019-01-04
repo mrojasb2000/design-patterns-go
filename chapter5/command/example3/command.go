f9b007eefa8343d54ca3cdb5cc77baa0f6e77570
Mauricio Rojas <mrojasb2000@gmail.com>
2019-01-04 16:45:53 -0300

    Chain of responsability of Commands patterns.

---
+++ chapter5/command/example3/command.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Command interface {
+	Info() string
+}
+
+type TimePassed struct {
+	start time.Time
+}
+
+func (t *TimePassed) Info() string {
+	return time.Since(t.start).String()
+}
+
+type ChainLogger interface {
+	Next(Command)
+}
+
+type Logger struct {
+	NextChain ChainLogger
+}
+
+func (l *Logger) Next(c Command) {
+	time.Sleep(time.Second)
+
+	fmt.Printf("Elapsed time from creation: %s\n", c.Info())
+
+	if l.NextChain != nil {
+		l.NextChain.Next(c)
+	}
+}
+
+func main() {
+	second := new(Logger)
+	first := Logger{NextChain: second}
+
+	command := &TimePassed{start: time.Now()}
+
+	first.Next(command)
+}
