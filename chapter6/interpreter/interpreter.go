ce3a9d2f7c58f6fadcb7c623110c209e93b5be30
Mauricio Rojas <mrojasb2000@gmail.com>
2019-01-09 16:29:50 -0300

    Interpreter Patterns and Unit Test.

---
+++ chapter6/interpreter/interpreter.go
@@ -0,0 +1,9 @@
+package interpreter
+
+import (
+	"fmt"
+)
+
+func Calculate(o string) (int, error) {
+	return 0, fmt.Errorf("Not implemented yet")
+}
---
+++ chapter6/interpreter/interpreter_test.go
@@ -0,0 +1,18 @@
+package interpreter
+
+import (
+	"testing"
+)
+
+func TestCalculate(t *testing.T) {
+	tempOperation := "3 4 sum 2 sub"
+
+	res, err := Calculate(tempOperation)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res != 5 {
+		t.Errorf("Expected result not found: %d != %d\n", 5, res)
+	}
+}
