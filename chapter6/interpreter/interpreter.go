45793293287de99ea68131944b3649a1c051a13b
Mauricio Rojas <mrojasb2000@gmail.com>
2019-01-09 17:08:47 -0300

    Implement Interpreter Patterns and testing.

--- chapter6/interpreter/interpreter.go
+++ chapter6/interpreter/interpreter.go
@@ -1,9 +1,83 @@
 package interpreter
 
 import (
-	"fmt"
+	"strconv"
+	"strings"
 )
 
+const (
+	SUM = "sum"
+	SUB = "sub"
+	MUL = "mul"
+	DIV = "div"
+)
+
+type polishNotationStack []int
+
+func (p *polishNotationStack) Push(s int) {
+	*p = append(*p, s)
+}
+
+func (p *polishNotationStack) Pop() int {
+	length := len(*p)
+
+	// get last items on stack
+	if length > 0 {
+		temp := (*p)[length-1]
+		*p = (*p)[:length-1]
+		return temp
+	}
+	return 0
+}
+
 func Calculate(o string) (int, error) {
-	return 0, fmt.Errorf("Not implemented yet")
+	stack := polishNotationStack{}
+	operators := strings.Split(o, " ")
+
+	for _, operatorString := range operators {
+		if isOperator(operatorString) {
+			right := stack.Pop()
+			left := stack.Pop()
+			mathFunc := getOperationFunc(operatorString)
+			res := mathFunc(left, right)
+			stack.Push(res)
+		} else {
+			// Is value
+			val, err := strconv.Atoi(operatorString)
+			if err != nil {
+				return 0, err
+			}
+			stack.Push(val)
+		}
+	}
+	return int(stack.Pop()), nil
+}
+
+func isOperator(o string) bool {
+	if o == SUM || o == SUB || o == MUL || o == DIV {
+		return true
+	}
+	return false
+}
+
+func getOperationFunc(o string) func(a, b int) int {
+	switch o {
+	case SUM:
+		return func(a, b int) int {
+			return a + b
+		}
+	case SUB:
+		return func(a, b int) int {
+			return a - b
+		}
+	case MUL:
+		return func(a, b int) int {
+			return a * b
+		}
+	case DIV:
+		return func(a, b int) int {
+			return a / b
+		}
+	}
+	return nil
 }
--- chapter6/interpreter/interpreter_test.go
+++ chapter6/interpreter/interpreter_test.go
@@ -4,15 +4,31 @@ import (
 	"testing"
 )
 
-func TestCalculate(t *testing.T) {
+func TestCalculate_3_4_sum_2_sub(t *testing.T) {
+	//􏰖􏰖􏰖􏱀Operation: (3 + 4) - 2
 	tempOperation := "3 4 sum 2 sub"
-
+	expected := 5
 	res, err := Calculate(tempOperation)
 	if err != nil {
 		t.Error(err)
 	}
 
-	if res != 5 {
-		t.Errorf("Expected result not found: %d != %d\n", 5, res)
+	if res != expected {
+		t.Errorf("Expected result not found: %d != %d\n", expected, res)
+	}
+}
+
+func TestCalculate_5_3_sub_8_mul_4_sum_5_div(t *testing.T) {
+	//􏰖􏰖􏰖􏱀Operation: (((5 - 3) * 8) + 4) / 5
+	tempOperation := "5 3 sub 8 mul 4 sum 5 div"
+
+	expected := 4
+	res, err := Calculate(tempOperation)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res != expected {
+		t.Errorf("Expected result not found: %d != %d\n", expected, res)
 	}
 }
