b905c72478521a70b19a7e7485fab2e992394de7
Mauricio Rojas <mrojasb2000@gmail.com>
2019-01-30 17:09:43 -0300

    Ranging over channels too, implement.

--- chapter8/08_18.go
+++ chapter8/08_18.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"time"
+)
+
 func main() {
+
+	ch := make(chan int)
+
+	go func() {
+		ch <- 1
+		time.Sleep(time.Second)
+
+		ch <- 2
+
+		close(ch)
+	}()
+
+	for v := range ch {
+		println(v)
+	}
 }
